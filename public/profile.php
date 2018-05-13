8d5c26957686756601d52f19a261ffddf12239f8
Juli Cheng <94julicheng@gmail.com>
2018-05-13 22:18:03 +0100

    Change profile from procedural to oop

--- public/profile.php
+++ public/profile.php
@@ -14,16 +14,17 @@ if(!isset($_GET['id'])) {
 }
 
 $id = $_GET['id'];
+$user = User::find_by_id($id);
 
 if(is_post_request()) {
-    $user = [];
-    $user['id'] = $id;
-    $user['first_name'] = isset($_POST['first_name']) ? $_POST['first_name'] : "";
-    $user['last_name'] = isset($_POST['last_name']) ? $_POST['last_name'] : "";
-    $user['email'] = isset($_POST['email']) ? $_POST['email'] : "";
-    $user['password'] = isset($_POST['password']) ? $_POST['password'] : "";
-    $user['confirm_password'] = isset($_POST['confirm_password']) ? $_POST['confirm_password'] : "";
-    $user['profile_img'] = "";
+    $args = [];
+    $args['id'] = $id;
+    $args['first_name'] = $_POST['first_name'] ?? "";
+    $args['last_name'] = $_POST['last_name'] ?? "";
+    $args['email'] = $_POST['email'] ?? "";
+    $args['password'] = $_POST['password'] ?? "";
+    $args['confirm_password'] =$_POST['confirm_password'] ?? "";
+    $args['profile_img'] = "";
 
     if($_FILES['profile_img']['size'] !== 0) {        
         $file = $_FILES['profile_img'];
@@ -35,47 +36,50 @@ if(is_post_request()) {
         }
     }
 
-    $result = update_user($user, $file_errors);
+    $user->merge_attributes($args);
+    $result = $user->save();
+
     if ($result === true) {
          $_SESSION['message'] = 'Your profile has updated successfully.';
         redirect_to(url_for('/profile.php?id=' . $_SESSION['id']));
     } else {
-        $user = find_user_by_id($id);
-        $errors = $result;
+        $user = User::find_by_id($id);
+        // $user->errors = $result;
     }
 
 } elseif(is_get_request()) {
-    $user = find_user_by_id($id);
+    // $user = User::find_by_id($id);
 }
 
 ?>
 
 <div class="content">
-<div class="profile-section">
-    <div class="profile-image">
-        <img src="<?php echo url_for('/images/' . $user['profile_img']); ?>">
+    <div class="profile-section">
+        <div class="profile-image">
+            <img src="<?php echo url_for('/images/' . $user->profile_img); ?>">
+        </div>
+        <div class="profile">
+            <h1>Your Profile</h1>
+            <h4>First Name: <span><?php echo ucfirst($user->first_name); ?></span></h4> 
+            <h4>Last Name: <span><?php echo ucfirst($user->last_name); ?></span></h4>
+            <h4>Email: <span><?php echo $user->email; ?></span></h4>
+        </div>
     </div>
-    <div class="profile">
-        <h1>Your Profile</h1>
-        <h4>First Name: <span><?php echo ucfirst($user['first_name']); ?></span></h4> 
-        <h4>Last Name: <span><?php echo ucfirst($user['last_name']); ?></span></h4>
-        <h4>Email: <span><?php echo $user['email']; ?></span></h4>
-    </div>
-</div>
+
     <hr>
 
     <div class="profile-errors">
-        <?php echo display_errors($errors); ?>
+        <?php echo display_errors($user->errors); ?>
     </div>
 
-    <form action="<?php echo 'profile.php?id=' . $user['id']; ?>" method="post" class="form_update" enctype="multipart/form-data">
+    <form action="<?php echo 'profile.php?id=' . $user->id; ?>" method="post" class="form_update" enctype="multipart/form-data">
 
         First Name:
-        <input type="text" name="first_name" value="<?php echo ucfirst($user['first_name']); ?>" /><br /><br />
+        <input type="text" name="first_name" value="<?php echo ucfirst($user->first_name); ?>" /><br /><br />
         Last Name:
-        <input type="text" name="last_name" value="<?php echo ucfirst($user['last_name']); ?>" /><br /><br />
+        <input type="text" name="last_name" value="<?php echo ucfirst($user->last_name); ?>" /><br /><br />
         Email:
-        <input type="email" name="email" value="<?php echo $user['email']; ?>" /><br /><br />
+        <input type="email" name="email" value="<?php echo $user->email; ?>" /><br /><br />
         Password:
         <input type="password" name="password" value="" /><br /><br />
         Confirm Password:
@@ -86,7 +90,6 @@ if(is_post_request()) {
 
     </form>
     
-
 </div>
 
 <?php include(SHARED_PATH . '/footer.php'); ?>
